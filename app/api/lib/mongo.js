052bbfe52098b9dd88d0f33b82690e8858afb49f
Naimur Rahman Naim <naimekattor@gmail.com>
2025-05-11 17:24:41 +0600

    updated mongo js

--- app/api/lib/mongo.js
+++ app/api/lib/mongo.js
@@ -6,11 +6,11 @@ const options = {};
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
+if (!import.meta.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-if (process.env.NODE_ENV === "development") {
+if (import.meta.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
@@ -22,3 +22,16 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export default clientPromise;
+
+/* import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
+ */
